80417b3d6925615c6beae2e797ef5db3447a024d
Shubha Guha <shubha.g@gmail.com>
2021-09-22 22:38:39 +0200

    Populated form fields upon initiating update

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
@@ -14,8 +14,15 @@ const Form = ({ currentId, setCurrentId }) => {
         tags: '',
         selectedFile: ''
     });
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (post) {
+            setPostData(post);
+        }
+    }, [post]);
 
     const handleSubmit = (e) => {
         e.preventDefault(); // prevent browser refresh
