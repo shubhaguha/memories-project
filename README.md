02b3f09a4375502c1ab3ce6aa48f25148e4c414f
Shubha Guha <shubha.g@gmail.com>
2021-09-22 14:36:20 +0200

    Created and connected to database

--- README.md
+++ README.md
@@ -33,3 +33,13 @@ npm init -y
 npm install body-parser cors express mongoose nodemon
 # import libraries in index.js
 ```
+
+### Database
+
+The tutorial recommends setting up a MongoDB cluster in the cloud (for free): <https://www.mongodb.com/cloud/atlas>
+
+1. Sign in with Google.
+1. Create a shared cluster for free.
+1. Database Access > Create a database user.
+1. Network Access > Add current IP address.
+1. Databases > Connect > Connect your application > Copy to server/index.js, with database username and password created above in step 3.
--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const CONNECTION_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.wnnpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
