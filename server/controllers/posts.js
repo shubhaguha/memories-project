7c7ec7db59e23d52318a80dc68642711bd5ebb46
Shubha Guha <shubha.g@gmail.com>
2021-09-23 08:47:46 +0200

    Fixed server-size that should include post id in update data

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -34,7 +34,7 @@ export const updatePost = async (req, res) => {
         res.status(404).send('No post with that id');
     }
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
     res.json(updatePost);
 }
