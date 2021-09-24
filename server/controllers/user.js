dd10428856b3a9ed9745f63036d9c856559a9648
Shubha Guha <shubha.g@gmail.com>
2021-09-24 17:56:35 +0200

    Fixed signup controller bug

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -37,7 +37,7 @@ export const signup = async (req, res) => {
     try {
         const existingUser = await User.findOne({ email });
 
-        if (!existingUser) {
+        if (existingUser) {
             return res.status(400).json({ message: "User already exists." })
         }
 
