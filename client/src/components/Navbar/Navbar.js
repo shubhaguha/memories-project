fb06dc76bd9d342f7c048d27a600eb8a711b4d06
Shubha Guha <shubha.g@gmail.com>
2021-09-24 20:17:55 +0200

    Logged out when token expired

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
 import memories from '../../images/memories.png';
 import useStyles from './styles';
@@ -24,7 +25,13 @@ const Navbar = () => {
     useEffect(() => {
         const token = user?.token;
 
-        // JWT...
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) {
+                logout();
+            }
+        }
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
