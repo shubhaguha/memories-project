eab6b93ee5fa765b8188cf7b2a2090a053593cd5
Shubha Guha <shubha.g@gmail.com>
2021-09-27 16:19:28 +0200

    Added loading state and actions

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,13 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes";
+import { FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes";
 
 // eslint-disable-next-line
-export default (state = { posts: [] }, action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
     switch (action.type) {
-        case FETCH_ALL:
+        case START_LOADING:
+            return { ...state, isLoading: true };
+        case END_LOADING:
+            return { ...state, isLoading: false };
+            case FETCH_ALL:
             return {
                 ...state,
                 posts: action.payload.data,
