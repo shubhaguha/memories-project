50418b3a85717a4c3eb51c03887eeea5f345c282
Shubha Guha <shubha.g@gmail.com>
2021-09-28 11:27:23 +0200

    Fixed edit button on Post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -41,12 +41,19 @@ const Post = ({ post, setCurrentId }) => {
                     <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
                 </div>
                 {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-                    <div className={classes.overlay2}>
-                        <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(post._id)}>
-                            <MoreHorizIcon fontSize="medium" />
-                        </Button>
+                    <div className={classes.overlay2} name="edit">
+                    <Button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            setCurrentId(post._id);
+                        }}
+                        style={{ color: 'white' }}
+                        size="small"
+                    >
+                        <MoreHorizIcon fontSize="default" />
+                    </Button>
                     </div>
-                )}
+              )}
                 <div className={classes.details}>
                     <Typography variant="body2" color="textSecondary">
                         {post.tags.map((tag) => `#${tag} `)}
